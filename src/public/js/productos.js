6d714649ffc4d7cda2d37909cacfa574f7b810f8
Juan-Balugano <juanbalugano@gmail.com>
2024-09-05 21:35:22 -0300

    version 1.0.1

--- src/public/js/productos.js
+++ src/public/js/productos.js
@@ -34,7 +34,6 @@ const eliminar = async ( pid) => {
 
         console.log(`Intentando eliminar el producto con PID: ${pid} del carrito con CID: ${cid}`);
 
-        // Construir la URL y hacer una depuración de la URL generada
         const url = `/api/carts/${cid}/products/${pid}`;
         console.log("URL construida:", url);
 
@@ -45,7 +44,7 @@ const eliminar = async ( pid) => {
         console.log(`Respuesta recibida:`, respuesta);
 
         if (respuesta.status === 200) {
-            let datos = await respuesta.json(); // Esto lanzará un error si la respuesta no es JSON válido
+            let datos = await respuesta.json(); 
             console.log("Datos de respuesta:", datos);
             alert("Producto eliminado con éxito.");
             window.location.reload();
@@ -59,28 +58,12 @@ const eliminar = async ( pid) => {
     }
 };
 
-//LOGICA DE CERRAR SESION 
-document.getElementById('logoutBtn').addEventListener('click', function() {
-    if (confirm('¿Estás seguro de que deseas cerrar sesión?')) {
-        // Si el usuario confirma el cierre de sesión
-        fetch('http://localhost:8080/api/sessions/logout', {
-            method: 'POST', 
-        })
-        .then(response => {
-            window.location.href = '/login'; //
-        })
-        .catch(error => {
-            console.error('Error al cerrar sesión:', error);
-        });
-    } 
-});
-
 const finalizarCompra = async () => {
-    console.log("finalizarCompra function called"); // Agrega este mensaje para depuración
+    console.log("finalizarCompra function called");
 
     let inputCarrito = document.getElementById("inputCarrito");
     if (!inputCarrito) {
-        console.error("inputCarrito no encontrado"); // Agrega este mensaje
+        console.error("inputCarrito no encontrado"); 
         alert("No se pudo encontrar el inputCarrito.");
         return;
     }
@@ -97,7 +80,7 @@ const finalizarCompra = async () => {
         let respuesta = await fetch(`/api/carts/${cid}/purchase`, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json" // Asegúrate de establecer los encabezados apropiados
+                "Content-Type": "application/json" 
             }
         });
 
@@ -115,98 +98,19 @@ const finalizarCompra = async () => {
     }
 };
 
+//LOGICA DE CERRAR SESION 
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    if (confirm('¿Estás seguro de que deseas cerrar sesión?')) {
+        fetch('http://localhost:8080/api/sessions/logout', {
+            method: 'POST', 
+        })
+        .then(response => {
+            window.location.href = '/login'; //
+        })
+        .catch(error => {
+            console.error('Error al cerrar sesión:', error);
+        });
+    } 
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const restar = async (cid, pid, quantity) => {
-    console.log(cid)
-    console.log(pid)
-    console.log(quantity)
-    if (quantity == 1) {
-        console.log("no se puede restar a 1")
-        return
-    }
-
-    let data = {
-        cantidad: -1
-    }
-    //Al hacer fetch tengo que configurar tambien los headers y el contenido que va a viajar en el body
-    let respuesta = await fetch(`/api/carts/${cid}/products/${pid}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });    
-
-    if(respuesta.ok) {
-        console.log("cantidad actualizada")
-        //Extraigo solo el json que devuelve mi api, que en este caso es la cantidad (sino me devuelve un choclo de cosas)
-        let resultado = await respuesta.json()
-        console.log(resultado)
-        let listQuantity = document.getElementById(`cartQuantity-${cid}-${pid}`)
-        listQuantity.innerHTML = `<li id="cartQuantity-${cid}-${pid}">Cantidad: ${resultado} <button id="cartButtonRestar" onclick="restar('${cid}', '${pid}', '${resultado}')">-</button> <button id="cartButtonSumar" onclick="sumar('${cid}', '${pid}')">+</button> </li>`        
-    } else {
-        console.log("error al actualizar cantidad")
-    }
-    
-}
-
-const sumar = async (cid, pid) => {
-    console.log(cid)
-    console.log(pid)
-
-    let data = {
-        cantidad: 1
-    }
-    //Al hacer fetch tengo que configurar tambien los headers y el contenido que va a viajar en el body
-    let respuesta = await fetch(`/api/carts/${cid}/products/${pid}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });    
-
-    if(respuesta.ok) {
-        console.log("cantidad actualizada")
-        //Extraigo solo el json que devuelve mi api, que en este caso es la cantidad (sino me devuelve un choclo de cosas)
-        let resultado = await respuesta.json()
-        console.log(resultado)
-        let listQuantity = document.getElementById(`cartQuantity-${cid}-${pid}`)
-        listQuantity.innerHTML = `<li id="cartQuantity-${cid}-${pid}">Cantidad: ${resultado} <button id="cartButtonRestar" onclick="restar('${cid}', '${pid}', '${resultado}')">-</button> <button id="cartButtonSumar" onclick="sumar('${cid}', '${pid}')">+</button> </li>`        
-    } else {
-        console.log("error al actualizar cantidad")
-    }
-}
-
-const vaciarCarrito = async (cid) => {
-    let respuesta = await fetch(`/api/carts/${cid}`, {method: "delete"})
-    if (respuesta.ok) {
-        const carrito = document.getElementById("cartUl")        
-        carrito.innerHTML = ""
-    }
-}
- */
